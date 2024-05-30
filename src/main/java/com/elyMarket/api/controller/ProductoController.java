1f94cbd01bb27a16bb2e45518020ea70ee335729
Troyano85 <josefernandotroyanop@gmail.com>
2024-05-30 18:59:56 -0500

    implementacion para subir la imagen al producto

--- src/main/java/com/elyMarket/api/controller/ProductoController.java
+++ src/main/java/com/elyMarket/api/controller/ProductoController.java
@@ -3,12 +3,15 @@ package com.elyMarket.api.controller;
 import com.elyMarket.api.model.Producto;
 import com.elyMarket.api.model.Usuario;
 import com.elyMarket.api.service.ProductoService;
+import com.elyMarket.api.service.UploadFileService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -18,6 +21,9 @@ public class ProductoController {
     private final Logger LOGGER= LoggerFactory.getLogger(ProductoController.class);
     @Autowired
     private ProductoService productoService;
+
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping()
     public String show(Model model){
@@ -32,10 +38,25 @@ public class ProductoController {
     }
 
     @PostMapping("/save")
-    public String save(Producto producto){
+    public String save(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("este es el odjeto producto{}",producto);
         Usuario u=new Usuario(1,"","","","","","","");
         producto.setUsuario(u);
+        //imagen
+        if(producto.getId()==null){//cuando se crea un producto
+            String nombreImagen=upload.saveImage(file);
+            producto.setImagen(nombreImagen);
+        }else {
+            if (file.isEmpty()){ //cuando editamos producto pero no cambiamos imagen
+                Producto p=new Producto();
+                p=productoService.get(producto.getId()).get();
+                producto.setImagen(p.getImagen());
+            }else{
+                String nombreImagen=upload.saveImage(file);
+                producto.setImagen(nombreImagen);
+            }
+        }
+
         productoService.save(producto);
         return "redirect:/productos";
     }
